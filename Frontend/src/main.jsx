c9c5604df22d1c533883e45a77e021ec80a422e5
shishpal <shishpalpolampally@gmail.com>
2025-12-07 14:16:04 +0530

    created router provider and tailwind base

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AppRouter from './routes/AppRouter';
+import './styles/tailwind.css';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
